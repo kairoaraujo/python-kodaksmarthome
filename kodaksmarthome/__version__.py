7b4e0c6fecb7d53ec0238d92500b6bef2349a37a
Kairo Araujo <kairo@kairo.eti.br>
2021-06-16 08:30:53 +0200

    Update __version__.py

--- kodaksmarthome/__version__.py
+++ kodaksmarthome/__version__.py
@@ -7,7 +7,7 @@ Copyright (c) 2019, 2020 Kairo de Araujo
 __title__ = "python-kodaksmarthome"
 __description__ = "Python Kodak SmartHome API"
 __url__ = "https://github.com/kairoaraujo/kodak_smarthome_api"
-__version__ = "devel"
+__version__ = "0.2.0-devel"
 __author__ = "Kairo de Araujo"
 __author_email__ = "kairo@dearaujo.nl"
 __license__ = "Apache 2.0"
