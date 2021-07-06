5f92782400ae5e7dec41b56f7c9d8081de5c861b
raghavrithvik <raghav.glass@gmail.com>
2021-07-06 08:31:09 +0530

    Create _version_.py

---
+++ kodaksmarthome-hk/_version_.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2019, 2020 Kairo de Araujo
+"""
+
+__title__ = "python-kodaksmarthome-hk"
+__description__ = "Python Kodak SmartHome HK API"
+__url__ = "https://github.com/kairoaraujo/kodak_smarthome_api"
+__version__ = "0.2.0-devel"
+__author__ = "Kairo de Araujo"
+__author_email__ = "kairo@dearaujo.nl"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2019, 2020 Kairo de Araujo"
