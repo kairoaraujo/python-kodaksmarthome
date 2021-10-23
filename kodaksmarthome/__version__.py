addee044790824a3db36f7a6b9b96e3fc575e804
Kairo Araujo <kairo@kairo.eti.br>
2021-10-23 12:23:41 +0200

    Release v0.1.4b0
    - Reduce the number of API calls

--- kodaksmarthome/__version__.py
+++ kodaksmarthome/__version__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2019, 2020 Kairo de Araujo
+Copyright (c) 2019-2021 Kairo de Araujo
 """
 
 __title__ = "python-kodaksmarthome"
 __description__ = "Python Kodak SmartHome API"
-__url__ = "https://github.com/kairoaraujo/kodak_smarthome_api"
-__version__ = "0.1.3beta2"
+__url__ = "https://github.com/kairoaraujo/python-kodaksmarthome"
+__version__ = "0.1.4beta0"
 __author__ = "Kairo de Araujo"
 __author_email__ = "kairo@dearaujo.nl"
 __license__ = "Apache 2.0"
-__copyright__ = "Copyright 2019, 2020 Kairo de Araujo"
+__copyright__ = "Copyright 2019-2021 Kairo de Araujo"
